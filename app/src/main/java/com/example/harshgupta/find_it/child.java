8a23fa97c2e9139bb34cbb43bd1cce277943491a
Harsh Gupta <hrshgpt22@gmail.com>
2017-03-24 14:58:14 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/harshgupta/find_it/child.java
@@ -0,0 +1,14 @@
+package com.example.harshgupta.find_it;
+
+/**
+ * Created by harsh on 24/3/17.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class child extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.child);
+    }
+
+}
